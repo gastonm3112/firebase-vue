c31c2dde3f167e7d5b4d20315556d61b2c159c66
gastonm3112 <gastonm3112@gmail.com>
2021-02-19 07:52:52 -0300

    Funcionalidades de materialize completas

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -7,25 +7,29 @@
           ><i class="material-icons">menu</i></a
         >
         <ul class="right hide-on-med-and-down">
-          <li><a href="sass.html">Sass</a></li>
-          <li><a href="badges.html">Components</a></li>
-          <li><a href="collapsible.html">Javascript</a></li>
-          <li><a href="mobile.html">Mobile</a></li>
+          <li><a href="#">Sass</a></li>
+          <li><a href="#">Components</a></li>
+          <li><a href="#">Javascript</a></li>
+          <li><a href="#">Mobile</a></li>
         </ul>
       </div>
     </nav>
 
     <ul class="sidenav" id="mobile-demo">
-      <li><a href="sass.html">Sass</a></li>
-      <li><a href="badges.html">Components</a></li>
-      <li><a href="collapsible.html">Javascript</a></li>
-      <li><a href="mobile.html">Mobile</a></li>
+      <li><a href="#">Sass</a></li>
+      <li><a href="#">Components</a></li>
+      <li><a href="#">Javascript</a></li>
+      <li><a href="#">Mobile</a></li>
     </ul>
   </div>
 </template>
 
 <script>
-export default {};
+export default {
+  mounted() {
+    M.AutoInit();
+  },
+};
 </script>
 
 <style></style>
