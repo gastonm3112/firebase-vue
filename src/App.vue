eb0af229dd07189e045dc77fd2c61f37f4affcbb
gastonm3112 <gastonm3112@gmail.com>
2021-02-19 07:33:44 -0300

    Navbar importada a App

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,19 @@
 <template>
   <div id="nav">
+    <Navbar />
     <router-link to="/">Home</router-link> |
     <router-link to="/about">About</router-link>
     <router-view />
   </div>
 </template>
+
+<script>
+//@ es un atajo a la carpeta componets
+import Navbar from "@/components/Navbar.vue";
+
+export default {
+  components: {
+    Navbar,
+  },
+};
+</script>
