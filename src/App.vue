69b491dc147470e494026d0d33edfa2fab4eddd1
gastonm3112 <gastonm3112@gmail.com>
2021-02-20 08:45:56 -0300

    Eliminadas rutas de app.vue

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,7 @@
 <template>
   <div id="nav">
     <Navbar />
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+
     <router-view />
   </div>
 </template>
