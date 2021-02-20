4391094a3356597653e6a85a45590d98f8d3ac8c
gastonm3112 <gastonm3112@gmail.com>
2021-02-20 08:40:57 -0300

    Agregada Ruta a Proyectos

--- src/router/index.js
+++ src/router/index.js
@@ -14,7 +14,15 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
+  },
+  {
+    path: '/proyectos',
+    name: 'Proyectos',
+    // route level code-splitting
+    // this generates a separate chunk (proyectos.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "proyectos" */ '../views/Proyectos.vue')
+  },
 ]
 
 const router = createRouter({
