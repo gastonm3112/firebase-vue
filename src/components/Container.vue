e13e6255c11d1a40a69f42a80fdc56b4cd6f334b
gastonm3112 <gastonm3112@gmail.com>
2021-02-22 09:44:48 -0300

    Botones añadidos a Card

--- src/components/Container.vue
+++ src/components/Container.vue
@@ -3,14 +3,19 @@
     <div class="card horizontal">
       <div class="card-stacked">
         <div class="card-content">
+          <span class="card-title">Card Title</span>
           <p>
             I am a very simple card. I am good at containing small bits of
             information.
           </p>
         </div>
         <div class="card-action">
-          <a href="#">This is a link</a>
-          <a href="#">This is a link</a>
+          <div class="row">
+            <a class="col s6 waves-effect waves-light btn">EDITAR</a>
+            <a class="col s6 waves-effect waves-light btn red darken-1"
+              >ELIMINAR</a
+            >
+          </div>
         </div>
       </div>
     </div>
