90e8e1efdbd95eed98962d4dc35b78c598521f46
gastonm3112 <gastonm3112@gmail.com>
2021-02-22 09:20:57 -0300

    Card agregada a Container

--- src/components/Container.vue
+++ src/components/Container.vue
@@ -1,4 +1,21 @@
-<template> </template>
+<template>
+  <div class="col s12 m7">
+    <div class="card horizontal">
+      <div class="card-stacked">
+        <div class="card-content">
+          <p>
+            I am a very simple card. I am good at containing small bits of
+            information.
+          </p>
+        </div>
+        <div class="card-action">
+          <a href="#">This is a link</a>
+          <a href="#">This is a link</a>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
 
 <script>
 export default {};
