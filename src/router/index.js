fe421d85d9ad4ef1a5a2c852d9d82f6b9f77bd25
gastonm3112 <gastonm3112@gmail.com>
2021-02-24 08:31:49 -0300

    Creada nueva vista Created

--- src/router/index.js
+++ src/router/index.js
@@ -23,6 +23,14 @@ const routes = [
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "proyectos" */ '../views/Proyectos.vue')
   },
+  {
+    path: '/reg-projects',
+    name: 'Created',
+    // route level code-splitting
+    // this generates a separate chunk (create.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "reg-projects" */ '../views/Created.vue')
+  },
 ]
 
 const router = createRouter({
