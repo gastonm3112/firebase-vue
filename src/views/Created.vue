368a963ef2b7565e05c3de4b0b81e035ed92c497
gastonm3112 <gastonm3112@gmail.com>
2021-02-24 09:12:51 -0300

    Form recibe la informacion de los inputs

--- src/views/Created.vue
+++ src/views/Created.vue
@@ -4,40 +4,54 @@
       <form class="col s12">
         <div class="row">
           <div class="input-field col s12">
-            <input id="project_name" type="text" class="validate" />
+            <input
+              id="project_name"
+              type="text"
+              class="validate"
+              v-model="project.title"
+            />
             <label for="project_name">Nombre del Proyecto</label>
           </div>
           <div class="input-field col s12">
-            <input id="project_description" type="text" class="validate" />
+            <input
+              id="project_description"
+              type="text"
+              class="validate"
+              v-model="project.description"
+            />
             <label for="project_description">Descripción del Proyecto</label>
           </div>
           <p>
             <label>
-              <input type="checkbox" />
+              <input type="checkbox" v-model="project.langs" value="Html" />
               <span>HTML</span>
             </label>
           </p>
           <p>
             <label>
-              <input type="checkbox" />
+              <input type="checkbox" v-model="project.langs" value="Css" />
               <span>CSS</span>
             </label>
           </p>
           <p>
             <label>
-              <input type="checkbox" />
+              <input
+                type="checkbox"
+                v-model="project.langs"
+                value="JavaScript"
+              />
               <span>JavaScript</span>
             </label>
           </p>
           <p>
             <label>
-              <input type="checkbox" />
+              <input type="checkbox" v-model="project.langs" value="Vue" />
               <span>Vue</span>
             </label>
           </p>
           <p>
             <label>
-              <input type="checkbox" />
+              <input type="checkbox" v-model="project.langs" value="React" />
               <span>React</span>
             </label>
           </p>
@@ -51,6 +65,7 @@
           </button>
         </div>
       </form>
+      {{ project }}
     </div>
   </div>
 </template>
