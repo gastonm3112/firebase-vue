a9f0b5e7349b4b344891108f3dd8704474832d00
gastonm3112 <gastonm3112@gmail.com>
2021-03-04 08:51:54 -0300

    Se creo el formulario de registro de usuarios

---
+++ src/components/Register.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="container">
+    <h2>Registrar Usuario</h2>
+    <div class="row">
+      <form @submit.prevent="" class="col s12">
+        <div class="row">
+          <div class="input-field col s12">
+            <input id="email" type="email" class="validate" />
+            <label for="email">Email</label>
+          </div>
+        </div>
+        <div class="row">
+          <div class="input-field col s12">
+            <input id="password" type="password" class="validate" />
+            <label for="password">Password</label>
+          </div>
+        </div>
+        <div class="row">
+          <div class="input-field col s12">
+            <input id="password" type="password" class="validate" />
+            <label for="password">Confirmar Password</label>
+          </div>
+        </div>
+        <button
+          class="btn waves-effect waves-light"
+          type="submit"
+          name="action"
+        >
+          Submit
+          <i class="material-icons right">send</i>
+        </button>
+      </form>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
